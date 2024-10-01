38fc68837d0e3b2a23c98fee24dbddc0c2f97450
SHAIK.ZOHAIB <20481A5450@gecgudlavallerumic.in>
2024-10-01 17:20:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,8 @@
 import firebase_admin
-from firebase_admin import firestore, credentials, initialize_app
-from pydantic import BaseModel
 from fastapi import FastAPI,HTTPException
 
-cred = credentials.Certificate('Key.json')
-
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
-
 app = FastAPI()
-
-class CollectionRequest(BaseModel):
-    collection_name: str
 
 @app.get("/")
 def root():
     return {"Api is up and running": "True"}
-
-
-@app.post("/collections")
-def get_collection(request: CollectionRequest):
-    try:
-        collection_name = request.collection_name
-        docs = db.collection(collection_name).stream()
-        documents = [doc.to_dict() for doc in docs]
-        # print(documents)
-        return {"collection_name": collection_name, "documents": documents}
-    except Exception as e:
-        raise HTTPException(status_code=404, detail=str(e))
